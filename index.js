f7fab88d8f8b6073899ec6bd19453d1e501f906a
dawidlinek123 <56173086+dawidlinek123@users.noreply.github.com>
2020-10-04 20:31:02 +0200

    czy dziala

---
+++ index.js
@@ -0,0 +1,9 @@
+// Test
+
+const get_data = async () => {
+    const data_pre = await fetch("adreserwera");
+    const data = await data_pre.json();
+    return data;
+};
+
+get_data.then((e) => console.log(e));
