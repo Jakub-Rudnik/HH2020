a0795ae3ed7da9fe81d59125b80569da121e934d
D3vmotiv <czechpiotr2701@gmail.com>
2020-10-08 14:53:09 +0200

    🔨 Created expo-typescript project template

---
+++ app-mobile/App.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { View } from "react-native";
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export default () => <View>515</View>;
